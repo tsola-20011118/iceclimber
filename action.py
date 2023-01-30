e9a2edc54fb6c9a2779c16787701ae57bdde70a6
田中蒼宙 <tanakasola@tanakaaochuus-Mac-Studio.local>
2023-01-30 23:40:48 +0900

    0.0.2　上下移動できる！！

--- action.py
+++ action.py
@@ -3,41 +3,45 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(216, 16 * 3 * 8, fps=30);#216/12=18
+        pyxel.init(224, 16 * 3 * 8, fps=30);#224/12=18
         pyxel.load("action.pyxres")
         self.window = [];
         self.window.append(Window(0));
-        self.window.append(Window(-1 * 16 * 3 * 8));
-        self.windowNum = 2;
+        # self.window.append(Window(-16 * 3 * 8))
+        self.windowNum = 0;
+        
         self.player = Player();
         pyxel.run(self.update, self.draw);
-        
 
     def update(self):
+        if self.player.currentWindow + 1 >= self.windowNum:
+            self.window.append(Window(-1 * 16 * 3 * 8))
+            self.windowNum += 1;
         self.player.update()
         if self.player.windowChange == True:
-            for p in range(self.windowNum):
-                if self.window[p].y >= -1 * 16 * 3 * 8 and self.window[p].y <= 16 * 3 * 8:
-                    self.window[p].update();
+            if self.player.currentWindow != 0 : self.window[self.player.currentWindow - 1].update()
+            self.window[self.player.currentWindow].update()
+            if self.player.currentWindow + 1 <= self.windowNum: self.window[self.player.currentWindow + 1].update()
 
     def draw(self):
-        pyxel.cls(9);
-        pyxel.text(0, 0, str(self.player.actionTime), 0)
-        pyxel.text(0, 10, str(self.player.y), 0)
+        pyxel.cls(0);
+        pyxel.text(0, 0, str(self.player.currentWindow), 8)
+        pyxel.text(0, 10, str(self.windowNum), 8)
         for i in range(8 * 3):
             if i % 3 == 0:
-                pyxel.line(0, i * 16-1, 216, i * 16-1, 0);
+                pyxel.line(0, i * 16-1, 224, i * 16-1, 0);
             else:
-                pyxel.line(0, i * 16-1, 216, i * 16-1, 11);
-        for p in range(self.windowNum):
-            if self.window[p].y >= -1 * 16 * 3 * 8 and self.window[p].y <= 16 * 3 * 8:
-                self.window[p].draw()
+                pyxel.line(0, i * 16-1, 224, i * 16-1, 11);
+        # if self.window[self.currentWindow].y >= -1 * 16 * 3 * 8 and self.window[self.player.currentWindow].y <= 16 * 3 * 8:
+        # if self.player.currentWindow != 0: self.window[self.player.currentWindow - 1].draw()
+        self.window[self.player.currentWindow].draw()
+        # if self.player.currentWindow + 1 <= self.windowNum:self.window[self.player.currentWindow + 1].draw()
         self.player.draw()
 
 
 class Player:
     def __init__(self):
-        self.x = 216 / 2 - 6; #102
+        self.x = 224 / 2 - 6; #102
         self.y = 16 * 3 * 8 - 32;
         self.tempY = 0;
         # playerが右端にはみ出ている時のflag
@@ -57,6 +61,8 @@ class Player:
         self.head = 0
         # playerの画面遷移化の確認
         self.windowChange = False;
+        self.currentWindow = 0
+
 
     def update(self):
         self.windowMove()
@@ -65,7 +71,7 @@ class Player:
             self.action()
             if self.actionFlag == False:
                 # 上に上がる
-                self.moveUD();
+                self.moveUD()
                 # 右左に動く
                 self.moveRL();
                 # 左右にはみ出た時反対側に移動する
@@ -75,9 +81,9 @@ class Player:
     def draw(self):
         pyxel.blt(self.x, self.y, 0, self.imageNumX, self.imageNumY, 16, 16, 0)
         if self.moveOutL == True:
-            pyxel.blt(216 + self.x, self.y, 0, self.imageNumX, self.imageNumY, 16, 16, 0);
+            pyxel.blt(224 + self.x, self.y, 0, self.imageNumX, self.imageNumY, 16, 16, 0);
         if self.moveOutR == True:
-            pyxel.blt(self.x - 216, self.y, 0, self.imageNumX, self.imageNumY, 16, 16, 0);
+            pyxel.blt(self.x - 224, self.y, 0, self.imageNumX, self.imageNumY, 16, 16, 0);
         
 
     #TODO self.headを0に戻す処理
@@ -98,21 +104,21 @@ class Player:
             self.face = -1;
 
     def moveOut(self):
-        if self.x < 0 and self.x + 16 > 0:
+        if self.x < 0 and self.x + 12 > 0:
             self.moveOutL = True;
         if self.moveOutL == True:
-            if self.x < -16:
-                self.x = 216 + self.x;
+            if self.x < -12:
+                self.x = 224 + self.x;
                 self.moveOutL = False;
             if self.x > 0:
                 self.moveOutL = False;
-        if self.x < 216 and self.x + 16 > 216:
+        if self.x < 224 and self.x + 12 > 224:
             self.moveOutR = True;
         if self.moveOutR == True:
-            if self.x > 216:
-                self.x = self.x - 216
+            if self.x > 224:
+                self.x = self.x - 224
                 self.moveOutR = False;
-            if self.x < 216 - 16:
+            if self.x < 224 - 12:
                 self.moveOutR = False;
 
     def action(self):
@@ -127,13 +133,14 @@ class Player:
     def windowMove(self):
         if self.windowChange == False and self.y <= 0:
             self.windowChange = True
+            self.currentWindow += 1;
             self.tempY = self.y
         if self.windowChange == True:
             if 16 * 3 * 8 == self.y:
                 self.windowChange = False
             else:
                 self.y += 8
-        if self.windowChange == False and self.y >= 16 * 3 * 8 - 24:
+        if self.windowChange == False and self.y > 16 * 3 * 8 - 32:
             self.y -= 4
 
     def imageChange(self):
@@ -168,22 +175,25 @@ class Window:
         self.x = 0;
         self.y = y;
         self.back = self.Background(self.x, self.y)
-        self.jem = self.Jem(self.x, self.y, self.back.hole)
+        self.jem = self.Jem(self.x, self.y, self.back.ladder)
+        self.enemy = self.Enemy(self.x, self.y, self.back.ladder, self.jem.jem);
 
     def update(self):
         self.y += 8;
         self.back.update(self.y);
         self.jem.update(self.y)
+        self.enemy.update(self.y);
 
     def draw(self):
         self.back.draw()
         self.jem.draw()
+        self.enemy.draw();
 
     class Background:
         def __init__(self, x, y):
             self.x = x;
             self.y = y;
-            self.hole = [pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13)]
+            self.ladder = [pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), 13]
 
         def update(self,y):
             self.y = y
@@ -192,18 +202,18 @@ class Window:
             for y in range(24):
                 if y % 3 == 2:
                     for x in range(14):
-                        if x == self.hole[int(y / 3)]:
-                            pyxel.blt(self.x + x * 16, self.y + y * 16 - 8, 1, 48, 0, 16, 64, 0);
-                        else:
-                            pyxel.blt(self.x + x * 16, self.y + y * 16, 1, 0, 0, 16, 16, 0);
+                        if x == self.ladder[int((y - 2) / 3)]:
+                            pyxel.blt(self.x + x * 16, self.y + y * 16  - 56, 1, 48, 0, 16, 56, 8);
+                        pyxel.blt(self.x + x * 16, self.y + y * 16, 1, 0, 0, 16, 16, 0);
+
 
     class Jem:
-        def __init__(self, x, y, hole):
+        def __init__(self, x, y, ladder):
             self.x = x
             self.y = y
             self.jem = [pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13)]
             for p in range(8):
-                if self.jem[p] == hole[p]:
+                if self.jem[p] == ladder[p]:
                     self.jem[p] = pyxel.rndi(0,1)
                     self.jem[p] *= 13
 
@@ -217,27 +227,26 @@ class Window:
                         if x == self.jem[int(y / 3)]:
                             pyxel.blt(self.x + x * 16, self.y + y * 16, 1, 0, 16, 16, 16, 0)
 
-    # class Jem:
-    #     def __init__(self, x, y, hole):
-    #         self.x = x
-    #         self.y = y
-    #         self.jem = [pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(
-    #             1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13)]
-    #         for p in range(8):
-    #             if self.jem[p] == hole[p]:
-    #                 self.jem[p] = pyxel.rndi(0, 1)
-    #                 self.jem[p] *= 13
+    class Enemy:
+        def __init__(self, x, y, ladder, jem):
+            self.x = x
+            self.y = y
+            self.jem = [pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(
+                1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13), pyxel.rndi(1, 13)]
+            for p in range(8):
+                if self.jem[p] == ladder[p] or self.jem[p] == jem[p]:
+                    while self.jem[p] == ladder[p] or self.jem[p] == jem[p]:
+                        self.jem[p] = pyxel.rndi(1, 13);
 
-    #     def update(self, y):
-    #         self.y = y
+        def update(self, y):
+            self.y = y
 
-    #     def draw(self):
-    #         for y in range(24):
-    #             if y % 3 == 1:
-    #                 for x in range(14):
-    #                     if x == self.jem[int(y / 3)]:
-    #                         pyxel.blt(self.x + x * 16, self.y +
-    #                                   y * 16, 1, 0, 16, 16, 16, 0)
+        def draw(self):
+            for y in range(24):
+                if y % 3 == 1:
+                    for x in range(14):
+                        if x == self.jem[int(y / 3)]:
+                            pyxel.blt(self.x + x * 16, self.y + y * 16, 2, 0, 0, 16, 16, 0);
 
 
 App();
