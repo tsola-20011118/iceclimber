260e1fd884223b7cdc33c674369564af30be02a2
tsola <solatanaka2022@keio.jp>
2023-01-31 18:55:39 +0900

    0.0.7 fix: floor=0でactionできない問題の解決

--- action.py
+++ action.py
@@ -78,11 +78,12 @@ class App:
             pyxel.text(95, 170, "ICE CRIMER", 7)
             pyxel.text(60, 200, "GAME OVER : YORU SCORE is "+ str(self.score) , 7)
             pyxel.text(60, 270, "press space to REstart!!", 7)
-        # pyxel.text(0, 0, str(self.window[self.player.currentWindow].jem.jemFlag[int(self.player.floor)]), 7)
-        # pyxel.text(0, 10, str(self.gameMode), 7)
-        # pyxel.text(0, 20, str(self.player.life), 7)
-        # pyxel.text(0, 30, str(self.player.y), 0)
-        # pyxel.text(0, 40, str(self.player.head), 0)
+        pyxel.text(0, 0, str(self.window[self.player.currentWindow].jem.jemFlag[0]), 7)
+        pyxel.text(0, 10, str(self.player.windowChangeUP) +
+                   str(self.player.windowChangeDOWN), 7)
+        pyxel.text(0, 20, str(self.player.actionFlag), 7)
+        pyxel.text(0, 30, str(pyxel.btnp(pyxel.KEY_SPACE, 1, 1)), 0)
+        pyxel.text(0, 40, str(self.player.floor), 0)
         # pyxel.text(0, 50, str(self.player.ladderUP), 0)
         # pyxel.text(0, 100, str(self.window[self.player.currentWindow].ladder[4]), 8)
         # pyxel.text(0, 110, str(self.window[self.player.currentWindow].enemy[4].x), 8)
@@ -182,8 +183,7 @@ class Player:
         if self.windowChangeUP == False and self.windowChangeDOWN == False:
             self.floor = int(self.y / 16 / 3)
             self.ladderCheck(window, downwindow)
-            if self.floor != False:
-                self.action()
+            self.action()
             if self.actionFlag == False:
                 # 上に上がる
                 self.moveUD()
